80e1594d435a816c6faef1996e4f3cf578d54862
Japo Domingo <japodomingo@gmail.com>
2022-09-21 14:48:59 +1200

    FB-11185: disable ckeditor version check

--- nanospell/autoload.js
+++ nanospell/autoload.js
@@ -20,9 +20,9 @@
 
 'use strict';
 var nanospell = {
-	ckeditor: function(selector, settings) {
-		if (typeof(window.CKEDITOR) == undefined) {
-			setTimeout(function() {
+	ckeditor: function (selector, settings) {
+		if (typeof (window.CKEDITOR) == undefined) {
+			setTimeout(function () {
 				window.nanospell.ckeditor(selector, settings), 300
 			})
 		}
@@ -35,8 +35,8 @@ var nanospell = {
 		nanospell.wysiwyg.cke.inject_plugin(selector, settings);
 	},
 	spell_ajax_folder_path: null,
-	base_path: function() {
-		if (typeof(this.spell_ajax_folder_path) !== 'undefined' && this.spell_ajax_folder_path !== null) {
+	base_path: function () {
+		if (typeof (this.spell_ajax_folder_path) !== 'undefined' && this.spell_ajax_folder_path !== null) {
 			return this.spell_ajax_folder_path;
 		}
 		this.spell_ajax_folder_path = ''
@@ -59,29 +59,29 @@ var nanospell = {
 nanospell.wysiwyg = {};
 nanospell.wysiwyg.cke = {
 	list: {},
-	inject_plugin: function(selector, settings) {
+	inject_plugin: function (selector, settings) {
 		var plugin_name = 'nanospell'
 		var plugin_url = nanospell.base_path() + "nanospell.ckeditor/plugin.js"
-		if (typeof(CKEDITOR) == 'undefined') {
+		if (typeof (CKEDITOR) == 'undefined') {
 			return;
 		}
-		if (CKEDITOR.version < "4") {
-			console.log("nanospell can not work with old CKEditor instances with versions less than 4.  Your version is " + CKEDITOR.version + " !")
-			return;
-		}
+		// if (CKEDITOR.version < "4") {
+		// 	console.log("nanospell can not work with old CKEditor instances with versions less than 4.  Your version is " + CKEDITOR.version + " !")
+		// 	return;
+		// }
 		for (var i in CKEDITOR.instances) {
 			if (selector.toLowerCase() === 'all' || CKEDITOR.instances[i].element.$.id === selector) {
 				nanospell.wysiwyg.cke.list[i] = true;
 			}
 		}
 		CKEDITOR.plugins.addExternal(plugin_name, plugin_url, '');
-		CKEDITOR.plugins.load(plugin_name, function(plugins) {
+		CKEDITOR.plugins.load(plugin_name, function (plugins) {
 			for (var i in nanospell.wysiwyg.cke.list) {
 				var editor = CKEDITOR.instances[i];
 				editor.config.nanospell = settings;
 				editor.config.removePlugins += ',wsc,scayt';
 				plugins[plugin_name].init(editor)
-			 
+
 			}
 		});
 	}
